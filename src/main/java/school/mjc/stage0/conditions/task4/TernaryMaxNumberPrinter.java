ff2bf2d87959243fdf48a03899e2843ac47afe0a
Lapa <lapabig@gmail.com>
2023-05-16 17:20:47 +0600

    Updated

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int com;
+        int res;
+        com = first >= second ?
+                +first:
+                +second;
+        res = com >= third ?
+                +com:
+                +third;
+        System.out.println(res);
     }
 }
